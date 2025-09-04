114168167a2db3817b8653068ae88a3431626825
AWS-roido-san <lloyd.parnecio@awsys-i.com>
2025-09-04 11:23:38 +0800

    Feat#2: Task-priority

--- GitActivity/src/main/App.java
+++ GitActivity/src/main/App.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class App {
     private static TaskManager taskManager = new TaskManager();
 
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the To-Do List App!");
